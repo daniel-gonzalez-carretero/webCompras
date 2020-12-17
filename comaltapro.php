c4e7ef6641da79fb2c200abc0814b042b9339155
raquelalcazar <71818474+raquelalcazar@users.noreply.github.com>
2020-12-17 09:59:32 +0100

    Create comaltapro.php

---
+++ comaltapro.php
@@ -0,0 +1,54 @@
+<?php
+
+	include_once("funciones.php");
+	include_once("conexion.php");
+
+	try{
+
+		if(!isset($_POST) || empty($_POST)){
+			
+			$categorias = obtenerCategorias($conexion);
+?>
+
+	<h1>Alta de productos</h1>
+	<form name='altaProd' method='post'action='<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>'>
+		Nombre: <input type='text' name='nombre'><br><br>
+		Precio: <input type='number' name='precio'><br><br>
+		Categor&#237;a: <select name='categorias'>
+						<option selected='true' disabled>-</option>
+						<?php
+							foreach ($categorias as $categoria => $array) {
+								echo "<option value='" .$array["ID_CATEGORIA"] ."'>" .$array["NOMBRE"] ."</option>";
+						}
+						?>
+						</select><br><br>
+
+		<input type='submit' value='A&#241;adir producto' name='alta'>
+	</form>
+	
+<?php		
+		}else{
+
+			$id_prod = obtenerCodProd($conexion);
+			$nombre = $_REQUEST["nombre"];
+			$precio = $_REQUEST["precio"];
+			$id_cat = $_REQUEST["categorias"];
+
+			insertarProducto($conexion, $id_prod, $nombre, $precio, $id_cat);			
+			
+
+		}
+
+	}catch(PDOException $e){
+
+		echo "<p>Error: " . $e->getMessage() ."</p>";
+	}
+
+	$conn = null;
+			
+?>
+			
+			</div>
+		</div>
+	</body>
+</html>
