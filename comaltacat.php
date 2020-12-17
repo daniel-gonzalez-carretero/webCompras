af67e7d2bbb8df4db0adc47d9435af205b636c8e
msg1999 <71818408+msg1999@users.noreply.github.com>
2020-12-17 18:49:29 +0100

    Add files via upload

---
+++ comaltacat.php
@@ -0,0 +1,27 @@
+<!--Marco Santiago-->
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+        <div>Datos de Registro categorias</div>
+		<form name='formulario' action='' method='post'>
+            <label for="categoria">Categoria</label>
+            <input type="text" name="categoria" /> <br />
+            <?php
+                include_once("php/conexion.php");
+                include_once("php/funciones.php");
+                if (!isset($_POST) || empty($_POST)) {
+            ?>
+            <?php
+                }
+                else {
+                    $categoria=$_POST["categoria"];
+                    $codigo=gen_cod_cat($conexion);
+                    altacat($conexion,$categoria,$codigo);
+                }
+            ?>
+            <input type="submit" name="enviar" id="enviar" value="Enviar">
+		</form>
+    </body>
+</html>
