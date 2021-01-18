ed23cc18ed01841d1bbb50ee9127eee9e0f5b649
raquelalcazar <71818474+raquelalcazar@users.noreply.github.com>
2021-01-18 12:20:33 +0100

    Update comconscom.php

--- comconscom.php
+++ comconscom.php
@@ -3,6 +3,9 @@
     Código por:         Raquel Alcázar
     Refactorizado por:  Daniel González Carretero
 -->
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -18,19 +21,18 @@
 	<?php 
 		include_once("funciones.php");
 		$clientes = obtenerClientes();
+
+		if ( !(isset($_SESSION) && !empty($_SESSION) && isset($_SESSION["usuario"])) ) {
+			// Si el usuario NO ha iniciado sesión (NO existe una cookie 'usuario')
+		?>
+
+			<p>Parece que no has iniciado sesión aún... Haz click <a href="iniciarSesion.php">aquí para iniciar sesión</a>, o <a href="comaltacli.php">aquí para registrarte</a> si aún no lo has hecho.</p>
+
+		<?php
+		} else {
 	?>
 
 	<form method='post' action='<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>'>
-		<label for="cliente">Cliente: </label>
-		<select name='cliente' required>
-			<option selected disabled>Selecciona un Cliente</option>
-			<?php
-				foreach($clientes as $cliente) {
-                    echo "<option value='". $cliente["NIF"] ."'>[". $cliente["NIF"] ."]: ". $cliente["NOMBRE"] ." ". $cliente["APELLIDO"] ."</option>";
-                }
-			?>
-		</select><br>
-
 		<label for="fecha_desde">Desde... </label>
 		<input type='date' name='fecha_desde' required><br>
 
@@ -40,15 +42,16 @@
 		<input type='submit' value='Ver compras' name='alta'>
 	</form>
 	
-<?php		
-		if (isset($_POST) && !empty($_POST)) {
-			$nif = $_POST["cliente"];
+<?php
+		if ( (isset($_POST) && !empty($_POST))) {
+			$nif = $_SESSION["usuario"];
 			$fecha_desde = $_POST["fecha_desde"];
 			$fecha_hasta = $_POST["fecha_hasta"];
 			
 			$comprasCliente = consultarCompras($nif, $fecha_desde, $fecha_hasta);		
 			verCompras($comprasCliente); // La función ya trata los posibles errores
 		}
+	}
 ?>
 			
 </body>
