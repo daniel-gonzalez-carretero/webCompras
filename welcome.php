d762dd70f88c73f27cd1276a485126c17f8a9819
raquelalcazar <71818474+raquelalcazar@users.noreply.github.com>
2021-01-19 12:10:17 +0100

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -1,8 +1,26 @@
 <?php
+	session_start();
 	include_once("funciones.php");
 
-	$nif = $_COOKIE["usuario"];
-	$datos = obtenerNombre($nif);
+	if (isset($_SESSION) && !empty($_SESSION) && isset($_SESSION["usuario"])) {
+		// Si el usuario ha iniciado sesión
+
+		if (isset($_GET) && !empty($_GET) && isset($_GET["cerrarSesion"]) && $_GET["cerrarSesion"] == "true") {
+			// ... pero quiere cerrar sesión...
+			session_unset();
+			session_destroy();
+			// Se borra la cookie
+			header("location: iniciarSesion.php"); // Y se le manda a 'iniciarSesión.php'
+		}
+
+		// Si ha iniciado sesión, y no se indica que se cierre la sesión:
+		$nif = $_SESSION["usuario"];
+		$datos = obtenerNombre($nif);
+
+	} else {
+		// Si el usuario NO ha iniciado sesión
+		header("location: iniciarSesion.php"); // Se le manda a 'iniciarSesión.php'
+	}
 ?>
 
 <!DOCTYPE html>
@@ -14,8 +32,8 @@
 </head>
 <body>
 	<h1>Bienvenido <?php echo $datos["nombre"]; ?></h1>
-	<a href="iniciarSesion.php">Cerrar sesión</a>
-	<a href="comproC.php">Comprar productos</a>
-	<a href="comconscom.php">Consultar compras</a>
+	<li><a href="welcome.php?cerrarSesion=true">Cerrar sesión</a></li>
+	<li><a href="compro.php">Comprar productos</a></li>
+	<li><a href="comconscom.php">Consultar compras</a></li>
 </body>
 </html>
