a037a49640468996741a222fb97b79432dd6f2ce
raquelalcazar <71818474+raquelalcazar@users.noreply.github.com>
2021-01-19 20:18:41 +0100

    Update compro.php

--- compro.php
+++ compro.php
@@ -2,6 +2,9 @@
     Código por:         Raquel Alcázar 
     Refactorizado por:  Daniel González Carretero
 -->
+<?php
+	session_start();
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -20,16 +23,6 @@
 		$clientes = obtenerClientes();
 	?>
 	<form method='post' action='<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>'>
-		<label for="cliente">Cliente:</label>
-		<select name='cliente' required>
-			<option selected disabled>Selecciona un Cliente</option>
-			<?php
-				foreach($clientes as $cliente) {
-                    echo "<option value='". $cliente["NIF"] ."'>[". $cliente["NIF"] ."]: ". $cliente["NOMBRE"] ." ". $cliente["APELLIDO"] ."</option>";
-                }
-			?>
-		</select><br>
-
 		<label for="producto">Producto:</label>
 		<select name='producto' required>
 			<option selected disabled>Selecciona un Producto</option>
@@ -38,26 +31,93 @@
                     echo "<option value='". $producto["ID_PRODUCTO"] ."'>[". $producto["ID_PRODUCTO"] ."]: ". $producto["NOMBRE"] ."</option>";
                 }
 			?>
-		</select><br>
+		</select><br><br>
 
 		<label for="cantidad">Cantidad:</label>
-		<input type="number" name="cantidad" min="1" required></br>
+		<input type="number" name="cantidad" min="1" ></br></br>
 
-		<input type='submit' value='Comprar' name='comprar'>
+		<input type="submit" value="Comprar" name="comprar">
+		<input type="submit" value="Agregar a la Cesta" name="agregar">
+		<input type="submit" value="Limpiar la Cesta" name="limpiar">
 	</form>
 	<?php	
+		include_once("funciones.php");
+		if (isset($_POST) && !empty($_POST)){
+	  	// AGREGAR A LA CESTA DE LA COMPRA
+		if (isset($_POST["agregar"]) && !empty($_POST["agregar"])) {
 
-		if (isset($_POST) && !empty($_POST)) {
-			$id_producto = $_POST["producto"];
-			$nif_cliente = $_POST["cliente"];
-			$cantidad = $_POST["cantidad"];
+			if(isset($_POST["producto"])){
+				if($_POST["cantidad"]!=null){
 
-			if ( realizarCompraProducto($nif_cliente, $id_producto, $cantidad) ) {
-				echo "<p>La compra se ha realizado correctamente</p>";
-			} // Si la función devuelve FALSE, es la propia función quien devuelve el mensaje de error
+					$codigoProducto = $_POST["producto"];
+					$cantidadAgregar = $_POST["cantidad"];
+					$stock = consultarStockTotal($codigoProducto);
+
+		 			if (!isset($_SESSION["cesta"])){
+
+		 				if ($stock["cantidadProducto"]!=null && $stock["cantidadProducto"]>=$_POST["cantidad"]){
+
+		 					$cesta[$codigoProducto]=$cantidadAgregar;
+						  	$_SESSION["cesta"]=$cesta;
+		 				}else{
+		 					echo "<p>Lo sentimos, no hay stock suficiente..<p>";
+		 				}
+
+		 			}else{
+						$cesta=$_SESSION["cesta"];
+
+						if(array_key_exists($codigoProducto, $cesta)){
+
+							if($stock["cantidadProducto"]>=($cesta[$codigoProducto]+$cantidadAgregar)){
+								$cesta[$codigoProducto]+=$cantidadAgregar;
+							}else{
+								echo "<p>Lo sentimos, no se pueden añadir más unidades de ese producto...<p>";
+							}
+							
+						}else{
+
+							if($stock["cantidadProducto"]>=($cesta[$codigoProducto]+$cantidadAgregar)){
+								$cesta[$codigoProducto]=$cantidadAgregar;
+							}else{
+								echo "<p>Lo sentimos, no hay stock suficiente...<p>";
+							}
+						}
+
+						$_SESSION["cesta"]=$cesta;
+
+		 			}
+				}else{
+					echo "<p>Debes indicar una cantidad.</p>";
+				}
+			}else{
+				echo "<p>Debes seleccionar un producto.</p>";
+			}
+	
+		}else if(isset($_POST["comprar"]) && !empty($_POST["comprar"])){
+
+			if(isset($_SESSION["cesta"])){
+				$compraCorrecta=realizarCompra($_SESSION["cesta"], $_SESSION["usuario"]);
+
+				if($compraCorrecta){
+					$_SESSION["cesta"]=null;
+				}
+			}else{
+				echo "<p>Añade los productos que quieras comprar a la cesta.</p>";
+			}
+			
+
+		}else if(isset($_POST["limpiar"]) && !empty($_POST["limpiar"])){
+
+			$_SESSION["cesta"]=null;
+
 		}
+		
+		print_r($_SESSION["cesta"]);
+	}
 
-	?>	
+?>	
+
+
 </body>
 <!-- Cambios de Refactorización Realizados -->
 <!-- 
